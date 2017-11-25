d8c3e9f7b77b682d5ace6c125af8cda896aac906
Jeremy <jeraguilon@gmail.com>
2017-11-24 19:01:53 -0500

    Project homepage completed

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
 @app.route('/')
 def root():
-    return render_template('dashboard.html')
+    return render_template('homepage.html', page_name="Projects")
+
+
+@app.route('/projects/<project_name>')
+def project_management(project_name):
+	return render_template('project_management.html', page_name=project_name)
