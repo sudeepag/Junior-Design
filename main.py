6122e0860883cfad68e1b7445f7a0c81a34e45fa
Jeremy <jeraguilon@gmail.com>
2017-11-27 00:08:48 -0500

    Created dashboard 'concept'

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def root():
 @app.route('/projects/<project_name>')
 def project_management(project_name):
 	return render_template('project_management.html', page_name=project_name)
+
+@app.route('/analytics')
+def dashboard():
+	return render_template('dashboard.html', page_name="Analytics")
