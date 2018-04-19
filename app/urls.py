73357feec00978969d9a26da2b0907c6ee1d6cd8
Sudeep Agarwal <sudeep@lawn-128-61-60-243.lawn.gatech.edu>
2018-04-19 18:34:09 -0400

    Update wsgi interface

--- app/urls.py
+++ app/urls.py
@@ -11,7 +11,18 @@ import app.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    path('admin/', admin.site.urls),
+    url(r'^$', app.views.login, name='login'),
+    url(r'^$', app.views.logout, name='logout'),
+    url(r'^$', app.views.main, name='main'),
+    url(r'^$', app.views.new_project, name='new_project'),
+    url(r'^$', app.views.delete_project, name='delete_project'),
+    url(r'^$', app.views.new_goal, name='new_goal'),
+    url(r'^$', app.views.complete_goal, name='complete_goal'),
+    url(r'^$', app.views.revert_goal, name='revert_goal'),
+    url(r'^$', app.views.add_work, name='add_work'),
+    url(r'^$', app.views.project_management, name='project_management'),
+    url(r'^$', app.views.analytics, name='analytics'),
+    url(r'^$', app.views.register, name='register'),
+    url(r'^$', app.views.forgot, name='forgot'),
+    url(r'^$', app.views.load_user, name='load_user')
 ]
